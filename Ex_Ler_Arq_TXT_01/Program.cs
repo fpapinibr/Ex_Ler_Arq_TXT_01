f6c94a35c2ae330354b7343c6d824e63ead97527
fpapinibr <fpapini@gmail.com>
2022-02-25 11:28:14 -0300

    Add project files.

---
+++ Ex_Ler_Arq_TXT_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Ex_Ler_Arq_TXT_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            try
+            {
+                StreamReader sr = new StreamReader("D:\\Test.txt");//Instancia um Objeto StreamReader (Classe de Manipulação de Leitura de Arquivos)
+                line = sr.ReadLine(); //Faz a Leitura de uma linha do arquivo e atribui a string line
+                while (line != null)// Laço de Repetição para fazer a leitura de linhas do arquivo até o (EOF - Fim do Arquivo)
+                {
+                    Console.WriteLine(line);//Imprime o retorno do arquivo no Console
+                    line = sr.ReadLine(); //Faz a Leitura de linha do arquivo e atribui a string line
+                }
+                sr.Close();//Fecha o Arquivo
+                Console.WriteLine("Fim da Leitura do Arquivo");
+                Console.ReadLine();
+            }
+            catch (Exception e) // Tratamento de erro na abertura do arquivo
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executando o Bloco de Comando - Sem Erros");
+            }
+        }
+    }
+}
